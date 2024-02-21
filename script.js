3bdb337cba73c1fc89d6d8d54f523ebcd2fba8a8
git-uday-kiran <udaykiran0486@gmail.com>
2024-02-21 16:09:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const videoElements = document.querySelectorAll("video");
+
+videoElements.forEach((video) => {
+  video.addEventListener("click", () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
